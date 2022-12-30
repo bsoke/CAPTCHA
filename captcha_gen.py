c6998c07b04faad35c3806746b4feb93a71b9dbf
bsoke <55930386+bsoke@users.noreply.github.com>
2022-12-30 16:11:19 +0300

    rand_background

--- captcha_gen.py
+++ captcha_gen.py
@@ -2,6 +2,21 @@ from PIL import Image, ImageDraw, ImageFont
 import random
 import string
 
+r = random.randint(0, 255)
+g = random.randint(0, 255)
+b = random.randint(0, 255)
+
+# Use the random values to create a color in the RGB format
+rand_background = (r, g, b)
+
+
+colors = [
+    (255, 127, 0),  # orange
+    (0, 255, 0),  # green
+    (0, 0, 255),  # blue
+    (75, 0, 130),  # indigo
+]
+
 
 def generate_random_string(length):
     letters_and_digits = string.ascii_letters + string.digits
@@ -9,7 +24,7 @@ def generate_random_string(length):
 
 
 random_string = generate_random_string(8)
-image = Image.new('RGB', (200, 50), (255, 255, 255))
+image = Image.new('RGB', (200, 50), rand_background)
 draw = ImageDraw.Draw(image)
 
 # Noise, points
@@ -19,14 +34,6 @@ for i in range(500):
 font = ImageFont.truetype('arial.ttf', 36)
 text_width, text_height = draw.textsize(random_string, font=font)
 
-
-colors = [
-    (255, 127, 0),  # orange
-    (0, 255, 0),    # green
-    (0, 0, 255),    # blue
-    (75, 0, 130),   # indigo
-]
-
 chars = list(random_string)
 x, y = 0, 0
 
@@ -34,7 +41,7 @@ x, y = 0, 0
 for i, c in enumerate(chars):
     text_color = colors[i % len(colors)]
 
-    draw.text((x+(i*3), y), c, fill=text_color, font=font)
+    draw.text((x + (i * 3), y), c, fill=text_color, font=font)
 
     x += draw.textsize(c, font=font)[0]
 
