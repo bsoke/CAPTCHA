47901d17a729fbafbb4976b010c78911e6f6907f
bsoke <55930386+bsoke@users.noreply.github.com>
2023-01-26 04:10:57 +0300

    Create dirDecoderUpdate.py
    matrix ocr update

---
+++ dirDecoderUpdate.py
@@ -0,0 +1,16 @@
+import pytesseract
+from PIL import Image
+from scipy.ndimage import gaussian_filter
+from scipy.ndimage import median_filter
+
+#text = pytesseract.image_to_string(img)
+
+img = Image.open('matrix.png')
+#img = Image.fromarray(median_filter(np.array(img), size=5))
+#img = Image.fromarray(gaussian_filter(np.array(img), sigma=2))
+print(img)
+img = img.crop((0, 0, 600, 470))
+
+text = pytesseract.image_to_string(img, config='--psm 11 --oem 3 -c tessedit_char_whitelist=0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+
+print(text)
